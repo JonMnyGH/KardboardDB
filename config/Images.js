30bfe911f23b0f90622a0dc08ee3315a8f1bd73e
JonMnyGH <jonr.olson@gmail.com>
2022-02-17 15:22:22 -0500

    remove unused icon

--- config/Images.js
+++ config/Images.js
@@ -1,7 +1,6 @@
 export default {
   Elliot: require('../assets/images/Elliot.jpg'),
   PermissionsNotification: require('../assets/images/PermissionsNotification.png'),
-  DraftbitIcon: require('../assets/images/DraftbitIcon.png'),
   AppleIcon114x114: require('../assets/images/AppleIcon114x114.png'),
   Playstore: require('../assets/images/Playstore.png'),
   VerticalOnCorporateByLogaster: require('../assets/images/VerticalOnCorporateByLogaster.png'),
